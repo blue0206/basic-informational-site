95ab89f47dbd346901e44afbf0d4edb496a909dc
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-01-27 22:26:22 +0530

    Refactor file path strings in index.js to use single quotes for consistency

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ const path = require('path');
 
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
-        fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
             if (err) throw err;
             res.writeHead(200, { 'content-type': 'text/html' });
             res.end(data);
         });
     } else if (req.url === '/about') {
-        fs.readFile(path.join(__dirname, "public", "about.html"), (err, data) => {
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, data) => {
             if (err) throw err;
             res.writeHead(200, { 'content-type': 'text/html' });
             res.end(data);
