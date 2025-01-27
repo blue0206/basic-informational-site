7114cf7933d7b8a2b12e9386e3d74eb0555320ef
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-01-27 22:11:07 +0530

    Add 404 error handling to serve custom 404 page

--- src/index.js
+++ src/index.js
@@ -21,6 +21,12 @@ const server = http.createServer((req, res) => {
             res.writeHead(200, { 'content-type': 'text/html' });
             res.end(data);
         })
+    } else {
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, { 'content-type': 'text/html' });
+            res.end(data);
+        });
     }
 });
 
